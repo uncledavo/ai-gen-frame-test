41363fcb68c3c26cdecd87efda57853df9e192a5
Uncle Davo <117970371+uncledavo@users.noreply.github.com>
2024-02-03 14:40:10 +1100

    Export Echo

--- src/app/components/Echo.tsx
+++ src/app/components/Echo.tsx
@@ -1,7 +1,9 @@
-<img
+export default function Echo(){
+  <img
   src="https://imagedelivery.net/BXluQx4ige9GuW0Ia56BHw/2639523a-690b-47af-16ab-ca07697fd000/original"
   alt="Echo the Dolphin"
   width="400"
   height="300"
   className="margin-auto"
 />
+}
