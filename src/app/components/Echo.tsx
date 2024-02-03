c135a6495790882cdfcc3fd4913bd1f38b80a135
Uncle Davo <117970371+uncledavo@users.noreply.github.com>
2024-02-03 14:48:08 +1100

    Ensure Echo component returns a react element

--- src/app/components/Echo.tsx
+++ src/app/components/Echo.tsx
@@ -1,9 +1,13 @@
-export default function Echo(){
-  <img
-  src="https://imagedelivery.net/BXluQx4ige9GuW0Ia56BHw/2639523a-690b-47af-16ab-ca07697fd000/original"
-  alt="Echo the Dolphin"
-  width="400"
-  height="300"
-  className="margin-auto"
-/>
+function Echo() {
+  return (
+    <img
+      src="https://imagedelivery.net/BXluQx4ige9GuW0Ia56BHw/2639523a-690b-47af-16ab-ca07697fd000/original"
+      alt="Echo the Dolphin"
+      width="400"
+      height="300"
+      className="margin-auto"
+    />
+  );
 }
+
+export default Echo;
